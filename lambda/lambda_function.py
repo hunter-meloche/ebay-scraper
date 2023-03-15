240f59db360b4f1755407f45dfc336000272de10
Hunter Meloche <hunter35x@gmail.com>
2023-03-15 15:03:55 -0400

    added shipping costs and seller rating

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -61,10 +61,11 @@ def lambda_handler(event,context):
 
     # Define the SQL query to insert data into the table
     insert_query = """INSERT INTO listings 
-        ("itemId", "listingName", "price", "link", "keywords", 
-        "buyItNow", "priceMin", "priceMax", "itemCondition", 
+        ("listingId", "listingName", "totalPrice", "itemPrice", 
+        "shippingPrice", "link", "keywords", "buyItNow", 
+        "priceMin", "priceMax", "itemCondition", 
         "extraFilters", "dateTime") VALUES 
-        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
+        (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
 
     # Create cursor object to insert into DB
     cursor = conn.cursor()
@@ -98,16 +99,47 @@ def lambda_handler(event,context):
             title = title.text
 
         # Finds the price of the listing
-        price = listing.find("span", class_="s-item__price")
-        if price is None:
-            continue
+        itemPrice = listing.find("span", class_="s-item__price")
+        if itemPrice:
+            itemPrice = itemPrice.text.replace("$", "")
         else:
-            price = price.text
+            Print("Unable to find item price ofr this listing; skipping")
+            continue
+
+        # Finds shipping price of the listing
+        shipping = listing.find("span", class_="s-item__shipping s-item__logisticsCost")
+        if shipping: # Case for regular shipping
+            if shipping == "Free shipping":
+                shippingPrice = 0
+            else:
+                shippingMatch = re.search(r"\$(\d+(?:\.\d{1,2})?)", shipping)
+                if shippingMatch:
+                    shippingPrice = shippingMatch.group(1)
+                else:
+                    print("Unable to find shipping info for listing; skipping")
+                    continue
+        else:
+            shipping = listing.find("span", class_="s-item__dynamic s-item__freeXDays")
+            if shipping: # Case for free+fast shipping
+                shippingPrice = 0
+            else:
+                print("Unable to find shipping info for listing; skipping")
+                continue
+
+        # Finds seller rating
+        sellerInfo = listing.find("span", class_="s-item__seller-info-text")
+        sellerMatch = re.search(r"\((\d+)\) (\d+(\.\d+)?)%", sellerInfo)
+        if sellerMatch:
+            feedback_score = int(sellerMatch.group(1))
+            positive_feedback_percent = float(sellerMatch.group(2))
+        else:
+            print("Unable to find seller info for listing; skipping")
+            continue
 
         # Generates primary key from item ID in listing link
-        match = re.search(r"/(\d+)\?", link)
+        listingMatch = re.search(r"/(\d+)\?", link)
         if match:
-            itemId = match.group(1)
+            listingId = listingMatch.group(1)
             print(itemId)
         else:
             print("Unable to find item ID in link; skipping")
@@ -116,8 +148,11 @@ def lambda_handler(event,context):
         # Generates a timestamp for when the listing was found
         timestamp = datetime.now()
 
+        # Calculates total price by adding shipping cost to item price
+        totalPrice = itemPrice + shippingPrice
+
         # Compiles the listing data to be inserted to the database
-        entry = (int(itemId), title, price.replace("$", ""), link, \
+        entry = (int(listingId), title, itemPrice, link, \
             KEYWORDS, bool(int(BUY_IT_NOW)), PRICE_MIN, PRICE_MAX, \
             ITEM_CONDITION, EXTRA_FILTERS, timestamp)
 
